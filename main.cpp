fa3360e72647262eda16efa20181f024ade73ae2
Sam <sampadashrestha315@gmail.com>
2026-01-28 14:53:57 +0700

    sucess

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 using namespace std;
+#include "sorting.h"
 
 int main(int argc, char * argv[]) {
     
@@ -14,19 +15,23 @@ int main(int argc, char * argv[]) {
   cout<<"argc is "<<argc<<endl;
     int i,N;
     int *a;
+    a= new int [argc -1];
     for(i=1;i<argc;i++){
-        cout<<"argv[" << i<<"]" <<argv[i]<<endl;
-      
+      cout<<"argv[" << i<<"]" <<argv[i]<<endl;
+      a[i-1] = atoi(argv[i]); 
+      cout<<a[i-1]<<endl;
     }
-    /* Convert to numbers*/
+
     N=argc-1;
+
     cout<<"Before sorting:";
     display(a,N);
     
     sort(a,N);
     cout<<"After sorting:";
     display(a,N);
-    
+ 
+    delete []a;
     
     return 0;
 }
--- sorting.h
+++ sorting.h
@@ -1,3 +1,29 @@
-void swap(int &a,int &b);
-void sort(int *a,int N);
-void display(int *a,int N);
+#include <iomanip>
+void swap(int &a, int &b);
+void sort(int *a, int N);
+void display(int *a, int N);
+
+void swap(int &a,int &b){
+    int hold;
+    hold= a;
+    a=b;
+    b=hold;
+}
+
+void sort(int *a,int N){
+    int i,j;
+    for (j=0;j<N-1;j++){
+        for (i=0;i<N-1;i++){
+            if (a[i]>a[i+1]) 
+            {swap(a[i],a[i+1]);}
+    }
+    }
+        
+}
+
+void display(int *a,int N){
+    for (int i=0;i<N;i++){
+        cout<<setw(5)<<a[i];
+    }
+    cout<<endl;
+}
