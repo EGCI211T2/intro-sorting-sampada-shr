df975d4895577e404ad92a425e15942279e01198
Sam <sampadashrestha315@gmail.com>
2026-01-28 15:15:52 +0700

    changed

--- sorting.h
+++ sorting.h
@@ -10,7 +10,7 @@ void swap(int &a,int &b){
     b=hold;
 }
 
-void sort(int *a,int N){
+/*oid sort(int *a,int N){
     int i,j;
     for (j=0;j<N-1;j++){
         for (i=0;i<N-1;i++){
@@ -19,11 +19,24 @@ void sort(int *a,int N){
     }
     }
         
+}*/
+
+void sort(int *a, int N) {
+    int i, j;
+    // Outer loop: Tells the program how many passes to make
+    for (j = 0; j < N - 1; j++) {
+        // Inner loop: Does the actual swapping for one pass
+        for (i = 0; i < N - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+            }
+        }
+    }
 }
 
 void display(int *a,int N){
     for (int i=0;i<N;i++){
-        cout<<setw(5)<<a[i];
+        cout<<" "<<a[i];
     }
     cout<<endl;
 }
